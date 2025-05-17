4fe617b2ce87fb162f8cc34169d0961e7633f265
Ivo Brandao <IvoBrandao@users.noreply.github.com>
2025-05-17 20:16:10 +0200

    chore: 🔧  adds setup dependencies for ubuntu

---
+++ setup_ubuntu.sh
@@ -0,0 +1,4 @@
+
+sudo apt install git gcc g++ make cmake  qt6-base-dev qt6-tools-dev-tools libglib2.0-dev zlib1g-dev \
+ 	libusb-1.0-0-dev libboost-dev libfftw3-dev python3-dev libudev-dev pkg-config
+
